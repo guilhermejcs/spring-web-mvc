4e32cdf534b2c9accaaed3e78254821f3eaadb31
Guilherme Souza <guilhermejcamargos@gmail.com>
2021-01-05 22:46:50 -0300

    new jedi form

--- src/main/java/com/globallabs/springwebmvc/controller/JediControler.java
+++ src/main/java/com/globallabs/springwebmvc/controller/JediControler.java
@@ -1,15 +1,55 @@
 package com.globallabs.springwebmvc.controller;
 
+import com.globallabs.springwebmvc.model.Jedi;
+import com.globallabs.springwebmvc.repository.JediRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.Valid;
+
 
 @Controller
 public class JediControler {
+
+    @Autowired
+    private JediRepository repository;
+
     @GetMapping("/jedi")
     public ModelAndView jedi(){
+
         final ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("jedi");
+
+        modelAndView.addObject("allJedi", repository.getAllJedi());
+
         return modelAndView;
+    }
+
+    @GetMapping("/new-jedi")
+    public ModelAndView newJedi(){
+
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("new-jedi");
+        modelAndView.addObject("jedi",new Jedi());
+        return modelAndView;
+    }
+
+    @PostMapping("/jedi")
+    public String createJedi(@Valid @ModelAttribute Jedi jedi, BindingResult result){
+
+        if(result.hasErrors()){
+            return "new-jedi";
+        }
+
+        repository.add(jedi);
+
+        return "redirect:jedi";
     }
 }
